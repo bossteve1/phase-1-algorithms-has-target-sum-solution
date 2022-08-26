ec4d90765e985e74bae80360a5aa1823449b2b41
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-08-26 23:46:10 +0300

    completed tasks

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (const number of array) {
+     const complement = target - number;
+     if (seenNumbers[complement]) return true;
+     seenNumbers[number] = true;
+  }
+   // if we reach the end of the array, return false
+  return false;
 }
 
 /* 
@@ -7,7 +14,14 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode herecreate an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
